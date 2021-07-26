b4993473551567411be5689d4b4ae77d5b2740ac
Henry Majewski <henryjmajewski@icloud.com>
2021-07-26 13:32:50 -0400

    Initial Commit

---
+++ Three Letter Word HAckwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Three Letter Word HAckwich
+//
+//  Created by Henry Majewski on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Three Letter Word HAckwich/Three_Letter_Word_HAckwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Word_HAckwichApp.swift
+//  Three Letter Word HAckwich
+//
+//  Created by Henry Majewski on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Word_HAckwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
